ea2c712f39d7a549a6b7438e0fe4ebf50fced40a
Nandhakumar E <91084679+senandhakumar@users.noreply.github.com>
2021-10-04 22:50:59 +0530

    Add files via upload

---
+++ imgtend.py
@@ -0,0 +1,73 @@
+import numpy as np
+import tensorflow as tf
+import keras_preprocessing
+from PIL import Image
+from keras_preprocessing import image
+from keras_preprocessing.image import ImageDataGenerator
+from numpy import asarray
+
+TRAINING_DIR = "rps/"
+training_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+VALIDATION_DIR = "test/"
+validation_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = training_datagen.flow_from_directory(
+    TRAINING_DIR,
+    target_size=(150, 150),
+    class_mode='categorical',
+    batch_size=1
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    VALIDATION_DIR,
+    target_size=(150, 150),
+    class_mode='categorical',
+    batch_size=1
+)
+
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 150x150 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The third convolution
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.5),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(3, activation='softmax')
+])
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+history = model.fit(train_generator, epochs=10, steps_per_epoch=15, validation_data=validation_generator, verbose=1,
+                    validation_steps=1)
+
+model.save("rps.h5")
+#model = tf.keras.models.load_model('rps.h5')
+img = image.load_img('test/rock/rock01-003.png', target_size=(150, 150))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+images = np.vstack([x])
+classes = model.predict(images)
+print(classes)
